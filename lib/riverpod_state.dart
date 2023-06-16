dca0b156f1d3fca93ba1774ac5b97080004abb54
M Atheer <55885230+masreplay@users.noreply.github.com>
2023-06-16 23:58:55 +0300

    fix:

--- lib/riverpod_state.dart
+++ lib/riverpod_state.dart
@@ -1,5 +1,3 @@
 library riverpod_state;
 
-export 'src/async_data_value.dart';
-export 'src/async_notifier_mixin.dart';
-export 'src/async_value_extension.dart';
+export 'src/state.dart';
---
+++ lib/src/state.dart
@@ -0,0 +1,61 @@
+import 'dart:developer';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+sealed class AsyncX<T> {
+  const AsyncX();
+
+  factory AsyncX.data(T data) = AsyncXData<T>;
+
+  static Future<AsyncX<T>> idle<T>() => Future.value(const AsyncXIdle());
+}
+
+class AsyncXIdle<T> extends AsyncX<T> {
+  const AsyncXIdle();
+}
+
+class AsyncXData<T> extends AsyncX<T> {
+  final T data;
+  const AsyncXData(this.data);
+}
+
+mixin AsyncNotifier<T> on AutoDisposeAsyncNotifier<AsyncX<T>> {
+  // @useResult
+  Future<AsyncValue<AsyncX<T>>> handle(Future<T> Function() callback) async {
+    state = AsyncValue<AsyncX<T>>.loading();
+    try {
+      final data = await callback();
+      return state = AsyncValue<AsyncX<T>>.data(AsyncX.data(data));
+    } catch (error, stackTrace) {
+      if (kDebugMode) log(toString(), error: error, stackTrace: stackTrace);
+      return state = AsyncValue<AsyncX<T>>.error(error, stackTrace);
+    }
+  }
+
+  // @useResult
+  Future<AsyncValue<AsyncX<T>>> handleX(
+      Future<AsyncValue<AsyncX<T>>> Function() callback) async {
+    state = AsyncValue<AsyncX<T>>.loading();
+    try {
+      return state = await callback();
+    } catch (error, stackTrace) {
+      if (kDebugMode) log(toString(), error: error, stackTrace: stackTrace);
+      return state = AsyncValue<AsyncX<T>>.error(error, stackTrace);
+    }
+  }
+}
+
+extension AsyncValueExtension<T> on AsyncValue<AsyncX<T>> {
+  void whenDataOrError({
+    required Function(T data) data,
+    required Function(Object? error, StackTrace? stackTrace)? error,
+  }) {
+    whenOrNull(
+      data: (value) {
+        if (value is AsyncXData<T>) data(value.data);
+      },
+      error: error,
+    );
+  }
+}
